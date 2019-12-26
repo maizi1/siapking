f33545a9d25d6ddd0e7fb11a16f397205a50ba7d
maizi <guow1969@gmail.com>
2019-12-26 18:08:22 +0800

    主页图片资源替换

--- assets/script/home.ts
+++ assets/script/home.ts
@@ -1,11 +1,9 @@
-const i18n = require('LanguageData');
+
 const { ccclass, property } = cc._decorator;
 @ccclass
-export default class Helloworld extends cc.Component {
+export default class Home extends cc.Component {
     @property(cc.Node)
     gif: cc.Node;
-    @property(cc.Label)
-    i18nLabel: cc.Label;
     @property(cc.Label)
     score: cc.Label;
 
@@ -17,34 +15,20 @@ export default class Helloworld extends cc.Component {
     languagePointer: number = 0;
 
     onLoad() {
-        i18n.init('zh');
         this.score.string = cc.sys.localStorage.getItem('score') || 'F';
         this.handlerGif(this.gif.children);
         cc.audioEngine.playEffect(this.bgAudio, true);
     }
 
-    togglI18n(event: cc.Event) {
-        const languages = ['zh', 'en'];
-        cc.audioEngine.playEffect(this.clickAudio, false);
-        if (this.languagePointer + 1 === languages.length) {
-            this.languagePointer = 0;
-        } else {
-            this.languagePointer++;
-        }
-        i18n.init(languages[this.languagePointer]);
-        i18n.updateSceneRenderers();
-        this.i18nLabel.string = languages[this.languagePointer];
-    }
 
     handlerGif(nodes: Array<cc.Node>) {
         const callFunc = flipXToggle(nodes);
-        nodes[0].active = false;
         nodes[1].active = false;
 
         this.schedule(callFunc, 0.33);
 
         function flipXToggle(targetNodes: Array<cc.Node>) {
-            let isRight = true;
+            let isRight = false;
             return () => {
                 targetNodes[isRight ? 0 : 1].active = true;
                 targetNodes[isRight ? 1 : 0].active = false;
