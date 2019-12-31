06bf1e1c4a71de79edb2ef6dafe50a6b1ff2d859
maizi <guow1969@gmail.com>
2019-12-31 19:10:34 +0800

    多人pk匹配动画

--- assets/script/pk.ts
+++ assets/script/pk.ts
@@ -7,23 +7,81 @@
 // Learn life-cycle callbacks:
 //  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
 //  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
-import pkMatch from './pkMatch'
-const {ccclass, property} = cc._decorator;
+import pkMatch from './pkMatch';
+const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class Pk extends cc.Component {
-
     @property(cc.Node)
     match: cc.Node = null;
+    @property(cc.Node)
+    startNode: cc.Node = null;
+    @property(cc.Node)
+    out: cc.Node;
 
-    // LIFE-CYCLE CALLBACKS:
+    @property(cc.AudioClip)
+    readyAudio: cc.AudioClip = null;
+    @property(cc.AudioClip)
+    whistleAudio: cc.AudioClip = null;
+    @property(cc.AudioClip)
+    outAudio: cc.AudioClip = null;
 
-    onLoad () {
-        pkMatch(this.match)
+    isReady: boolean = false;
+
+    onLoad() {
+        // pkMatch(this.match, () => {
+        //     this.scheduleOnce(() => this.match.destroy(), 1);
+        // });
+        this.playStart();
     }
 
-    start () {
+    start() {}
 
+    playStart() {
+        const [ready, go] = this.startNode.children;
+        const { tween } = cc;
+        tween(this.startNode)
+            .to(0.6, { position: cc.v2(0, -35) })
+            .delay(1)
+            .to(0.6, { position: cc.v2(-670, -35) })
+            .start();
+        tween(ready)
+            .delay(0.8)
+            .to(0.2, { opacity: 0 })
+            .start();
+        tween(go)
+            .delay(0.9)
+            .to(0.3, { opacity: 255 })
+            .start();
+
+        this.scheduleOnce(() => {
+            cc.audioEngine.playEffect(this.readyAudio, false);
+            this.isReady = true;
+        }, 1.2);
+    }
+
+    onOut() {
+        const { out } = this;
+        const { children } = out;
+        cc.tween(out)
+            .to(0.6, { position: cc.v2(-550, -284) }, { easing: 'sineInOut' })
+            .start();
+        cc.tween(children[1])
+            .delay(1.2)
+            .by(0.4, { position: cc.v2(-380, 0) }, { easing: 'sineInOut' })
+            .start();
+        cc.tween(children[2])
+            .delay(1.2)
+            .by(0.4, { position: cc.v2(-380, 0) }, { easing: 'sineInOut' })
+            .start();
+        cc.tween(children[0])
+            .delay(1.6)
+            .by(0.4, { position: cc.v2(-380, 0) }, { easing: 'sineInOut' })
+            .start();
+        cc.audioEngine.playEffect(this.whistleAudio, false);
+        this.scheduleOnce(() => {
+            cc.audioEngine.playEffect(this.outAudio, false);
+        }, 0.8);
     }
 
     // update (dt) {}
--- assets/script/pkMatch.ts
+++ assets/script/pkMatch.ts
@@ -8,44 +8,74 @@
 //  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
 //  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
 
-function vs(node: cc.Node, callBack) {
+export default function play(node: cc.Node, callback: () => void) {
+    vs(node.children[0], () => match(node.children[1], callback));
+}
+
+function vs(node: cc.Node, callback: () => void) {
     const skeleton = node.getComponent(sp.Skeleton);
     const track = skeleton.setAnimation(0, 'animation', false);
 
-    skeleton.setTrackCompleteListener(track, callBack);
+    skeleton.setTrackCompleteListener(track, callback);
 }
 
-function match(node: cc.Node) {
+function match(node: cc.Node, callback: () => void) {
     const {
         children: [{ children: animations }, { children: icons }],
     } = node;
-    const { tween } = cc;
-    let icon1: cc.SpriteFrame, icon2: cc.SpriteFrame;
-    cc.loader.loadRes('icon/1', cc.SpriteFrame, (err, spr: cc.SpriteFrame) => {
-        if (!err) {
-            icon1 = spr;
-        }
-    });
+    const isLoad = [];
 
-    tween(icons[0])
-        .to(0.6, { scale: 1 })
-        .start();
-    tween(icons[1])
-        .to(0.6, { scale: 1 })
-        .start();
+    loadRes('icon/2', icons[2]);
+    loadRes('icon/3', icons[4]);
+    zoom(icons[0], icons[1]);
 
+    const random = Math.random() * 1 + 1.5;
+    const isDelay = Math.random() < 0.5 ;
     const skeleton1 = animations[0].getComponent(sp.Skeleton);
     const skeleton2 = animations[1].getComponent(sp.Skeleton);
 
-    const track1 = skeleton1.setAnimation(0, 'Match1', true);
-    const track2 = skeleton2.setAnimation(0, 'Match1', true);
-
-    skeleton2.addAnimation(0, 'Match2', false, 5);
-    skeleton2.addAnimation(0, 'Match3', false, 0.5);
-    // skeleton1.setTrackCompleteListener(track1, callBack);
-    // skeleton2.setTrackCompleteListener(track2, callBack);
+    handlerSkeleton(skeleton1, icons[2], icons[3], isDelay ? random : 0, isLoad, callback);
+    handlerSkeleton(skeleton2, icons[4], icons[5], isDelay ? 0 : random, isLoad, callback);
 }
 
-export default function play(node: cc.Node) {
-    vs(node.children[0], () => match(node.children[1]));
+function handlerSkeleton(
+    skeleton: sp.Skeleton,
+    iconNode: cc.Node,
+    nameNode: cc.Node,
+    random: number,
+    isLoad: boolean[],
+    callback: () => void
+) {
+    skeleton.setAnimation(0, 'Match1', true);
+    skeleton.addAnimation(0, 'Match2', false, 1.5 + random);
+    const track = skeleton.addAnimation(0, 'Match3', false, 0.6);
+
+    skeleton.setTrackCompleteListener(track, () => {
+        zoom(iconNode, nameNode);
+        isLoad.push(true);
+        if (isLoad.length === 2) {
+            callback()
+        }
+    });
+}
+
+function zoom(node1: cc.Node, node2: cc.Node) {
+    const { tween } = cc;
+    tween(node1)
+        .to(0.6, { scale: 1 })
+        .start();
+    tween(node2)
+        .to(0.6, { scale: 1 })
+        .start();
+}
+
+function loadRes(path: string, node: cc.Node) {
+    let sprite: cc.SpriteFrame;
+    cc.loader.loadRes(path, cc.SpriteFrame, (err, spr: cc.SpriteFrame) => {
+        if (!err) {
+            node.getComponent(cc.Sprite).spriteFrame = spr;
+        }
+    });
+
+    return sprite;
 }
